53ac12f4251b1a562311a1f3c524f86c4c4059a6
roberto-palanga <r.palanga94@gmail.com>
2024-02-01 18:00:14 +0100

    Inserimento delle fuction

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,54 @@
+let buttonElement = document.querySelector("#start-game");
+
+let userChoice = document.querySelector("#user-choice");
+
+let pcChoice = document.querySelector("#pc-choice");
+
+let result = document.querySelector("#result")
+
+buttonElement.addEventListener(`click`,
+    function () {
+
+        let numberA = Math.floor(Math.random() * 6) + 1;
+        userChoice.innerHTML = numberA;
+        
+        let numberB = Math.floor(Math.random() * 6) + 1;
+        pcChoice.innerHTML = numberB;
+
+        if (numberA > numberB) {
+
+            result.innerHTML = "Hai vinto";
+            
+        } else if (numberA < numberB) {
+
+            result.innerHTML = "Hai perso";
+
+        } else (numberA === numberB) {
+
+            result.innerHTML = "Pareggio";
+
+        }
+        
+        console.log(result)
+        
+    }
+
+
+
+)
+
+
+
+
+
+
+
+// else (numberA < numberB) {
+
+//     result.innerHTML = "Hai perso";
+
+
+// }
+// 
+
 
